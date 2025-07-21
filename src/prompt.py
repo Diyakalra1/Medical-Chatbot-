d4d8adba155bf8bf007a00aa6ecb17aa3c1bc225
Diyakalra1 <158696444+Diyakalra1@users.noreply.github.com>
2025-07-21 17:44:19 +0530

    prompt template created

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+from langchain_core.prompts import PromptTemplate
+template="""
+use the following peice of information to answer the user's question. If you do not know the answer just simply say that you do not know the answer , do not try to make up an answer.
+Context:{context}
+Question={question}
+Only give the helpful answer below and nothing else.
+Helpful answer:
+"""
+PROMPT=PromptTemplate(template=template,input_variables=["context","question"])
